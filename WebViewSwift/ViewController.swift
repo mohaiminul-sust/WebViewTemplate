129102271deb58a84ffbc6318f6206b8079fd914
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-04-19 21:54:32 +0600

    Upload

--- WebViewSwift/ViewController.swift
+++ WebViewSwift/ViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UIScrollViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    let url = "https://alwayscreative.net/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let requestUrl = NSURL(string: url)
+        let request = NSURLRequest(URL: requestUrl!)
+        
+        //delegates
+        webView.delegate = self
+        webView.scrollView.delegate = self
+        
+        //load request
+        webView.dataDetectorTypes = UIDataDetectorTypes.All
+        webView.loadRequest(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Delegates: webView delegates
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        
+        //error viewing code
+        let alert = UIAlertController(title: "Alert", message: "\(error)", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        return true
+    }
+    
+    //Delegates: scrollView delegates
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return nil
     }
 
 
